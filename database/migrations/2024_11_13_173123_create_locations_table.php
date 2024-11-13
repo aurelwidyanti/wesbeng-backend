c345ee5631196f8e8ba4d9e2300531cf2c091ddb
Fachruddin <poenyoex@gmail.com>
2024-11-14 00:33:10 +0700

    feat: Refactor locations migration to define table structure with additional fields

--- database/migrations/2024_11_13_173123_create_locations_table.php
+++ database/migrations/2024_11_13_173123_create_locations_table.php
@@ -4,24 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateLocationsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('category');
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 10, 7);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('locations');
     }
-};
+}
