2a9e8c05a07de27ce800598266d47a5aac6fa580
Fachruddin <poenyoex@gmail.com>
2024-11-14 00:41:42 +0700

    feat: Update LocationSeeder to insert multiple location records into the database

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -1,19 +1,36 @@
 <?php
+namespace Database\Seeders;
 
 use App\Models\Location;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class LocationSeeder extends Seeder
 {
     public function run()
     {
-        Location::create([
-            'name' => 'Titik Pembuangan 1',
-            'description' => 'Area perumahan',
-            'category' => 'Sampah Tidak Bakar',
-            'latitude' => -6.1751,
-            'longitude' => 106.8650,
+        DB::table('locations')->insert([
+            [
+                'name' => 'Titik Pembuangan 1',
+                'description' => 'Area perumahan',
+                'category' => 'Sampah Tidak Bakar',
+                'latitude' => -6.1751,
+                'longitude' => 106.8650,
+            ],
+            [
+                'name' => 'Titik Pembuangan 2',
+                'description' => 'Area komersial',
+                'category' => 'Sampah Organik',
+                'latitude' => -6.2000,
+                'longitude' => 106.8450,
+            ],
+            [
+                'name' => 'Titik Pembuangan 3',
+                'description' => 'Area industri',
+                'category' => 'Sampah B3',
+                'latitude' => -6.3000,
+                'longitude' => 106.9000,
+            ],
         ]);
     }
 }
-
