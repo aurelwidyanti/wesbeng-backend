ad50947e9fbe6c8934e7e853d7b43a0776e2149f
Fachruddin <poenyoex@gmail.com>
2024-11-14 00:52:54 +0700

    feat: Enhance Location model with fillable attributes, categories, and query scopes

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -8,4 +8,58 @@ use Illuminate\Database\Eloquent\Model;
 class Location extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'locations';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'category',
+        'latitude',
+        'longitude',
+    ];
+
+    /**
+     * The available categories.
+     *
+     * @var array<int, string>
+     */
+    public static $categories = [
+        'Sampah Bakar',       
+        'Sampah Tidak Bakar',  
+        'Sampah Daur Ulang',  
+        'Sampah Besar'
+    ];
+
+    /**
+     * Scope a query to only include locations of a given category.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $category
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeByCategory($query, $category)
+    {
+        return $query->where('category', $category);
+    }
+
+    /**
+     * Get the coordinates as a string.
+     *
+     * @return string
+     */
+    public function getCoordinatesAttribute()
+    {
+        return "{$this->latitude}, {$this->longitude}";
+    }
 }
